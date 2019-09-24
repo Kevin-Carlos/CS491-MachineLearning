b793cf995e3d41330545fb48ac7f14c641ceb9d2
James Schnebly <jameschnebly@gmail.com>
2019-09-23 23:27:03 -0700

    I THINK I DID IT HOLY CRAP

--- decision_trees.py
+++ decision_trees.py
@@ -20,7 +20,7 @@ import math
 # Y = np.array([[1], [0], [0], [0], [1]]) # Training labels 1
 
 # Entropy Test Data
-X = np.array([[1, 1, 0, 0], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 0, 1], [0, 0, 1, 1], [0, 0, 1, 0], [0, 0, 0, 0], [1, 0, 1, 0], [1, 1, 1, 0], [0, 0, 1, 0]])
+X = np.array([[1, 1, 0, 0], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 0, 1], [0, 0, 1, 1], [0, 0, 1, 0], [0, 0, 0, 0], [1, 0, 1, 0], [1, 1, 1, 0], [0, 0, 1, 1]])
 Y = np.array([[0], [1], [1], [0], [0], [1], [0], [0], [1], [0]])
 
 
@@ -37,17 +37,132 @@ def calcEntropy(X, Y):
             yes += 1
 
     totalCount = no + yes
+
+    yesSide1 = (yes/totalCount) 
+    noSide1 = (no/totalCount) 
+
     try:
-        entropy = -(no/totalCount) * math.log2(no/totalCount) - (yes/totalCount) * math.log2(yes/totalCount)
-        return entropy
+        yesSide2 = math.log2(yes/totalCount)
     except:
-        return 0 
+        yesSide2 = 0
+
+    try:
+        noSide2 = math.log2(no/totalCount)
+    except:
+        noSide2 = 0
+
+    yesSide = yesSide1 * yesSide2
+    noSide = noSide1 * noSide2
+
+    entropy = -1 * noSide - yesSide
+    return entropy
+
 
 def calcIG(H, p_L, p_R, H_l, H_r):
     # Calculate IG for split
     summation = p_R * H_r + p_L * H_l
     IG = H - summation
     return IG
+
+def split(X, Y, max_depth, num_splits, featureList):
+    ### Base Cases
+    # ------------
+
+    # If max depth is hit OR the feature List is empty aka no more features to split on return leaf node classifying the higher percentage class
+    if num_splits == max_depth or not featureList:
+        no = 0
+        yes = 0
+        for item in Y:
+            if (item == 0):
+                no += 1
+            if (item == 1):
+                yes += 1
+        if yes >= no:
+            return ([1, 1], None, None)
+        else:
+            return ([1, 0], None, None)
+
+
+    H = calcEntropy(X, Y) # H()
+    print("H():", H)
+
+    # If Entropy == 0 return leaf node classifying the only class in the data
+    if H == 0:
+        return ([1, int(Y[0])], None, None )
+
+    ### END BASE CASES
+
+    highest_IG = -99999999
+
+    for num in featureList:
+        X_l = []
+        X_r = []
+        Y_l = []
+        Y_r = []
+        i = 0
+        trindex_l = []
+        trindex_r = []
+
+        for row in X:
+            # Subset Data to get L and R sides
+            if row[num] == 0:
+                # This row belongs in X_l
+                X_l.append(row)
+                Y_l.append(Y[i])
+
+            elif row[num] == 1:
+                # This row belongs in X_r
+                X_r.append(row)
+                Y_r.append(Y[i])
+            
+            i += 1
+
+        X_l = np.array(X_l)
+        X_r = np.array(X_r)
+        Y_l = np.array(Y_l)
+        Y_r = np.array(Y_r)
+
+        # Calculate probability of being on each side (row wise)
+        samples_r = X_r.shape[0]
+        samples_l = X_l.shape[0]
+        total_samples = samples_l + samples_r
+
+        p_L = samples_l / total_samples
+        p_R = samples_r / total_samples
+
+        # Calculate Entropy of each side
+        # print("Entropy on each side for feature", num + 1)
+
+        h_l = calcEntropy(X_l, Y_l)
+        h_r = calcEntropy(X_r, Y_r)
+
+        # print("\tEntropy for L:", h_l)
+        # print("\tEntropy for R:", h_r)
+        # Calculate the Information Gain For the Split
+        IG = calcIG(H, p_L, p_R, h_l, h_r)
+        # print("IG FOR FEATURE", num + 1, "SPLIT:", IG)
+
+        # Test if highest IG
+        if (IG > highest_IG):
+            highest_IG = IG
+            highest_IG_feature_num = num
+            best_X_l = X_l
+            best_X_r = X_r
+            best_Y_l = Y_l
+            best_Y_r = Y_r
+            # print("Feature number", num + 1, "has highest IG with a value of", IG)
+
+    num_splits += 1
+    # print("Splitting on Feature number", highest_IG_feature_num + 1)
+
+    # Take the feature out of the feature list
+    featureList.remove(highest_IG_feature_num)
+    # print(featureList)
+
+    # return a recursive call to fill out the tree
+    print("Samples Going Left:", best_X_l.shape[0])
+    print("Samples Going Right:", best_X_r.shape[0])
+    return ([0, highest_IG_feature_num + 1], split(best_X_l, best_Y_l, max_depth, num_splits, featureList), split(best_X_r, best_Y_r, max_depth, num_splits, featureList))
 
 #
 # X = feature data as a 2D array, each row is a single sample
@@ -59,95 +174,8 @@ def calcIG(H, p_L, p_R, H_l, H_r):
 #
 def DT_train_binary(X,Y,max_depth):
 
-    # H() = H
-    H = calcEntropy(X, Y)
+    DT = split(X, Y, max_depth, 0, list(range(X.shape[1])))
 
-    num_features = X.shape[1]
-    splits = 0
+    return(DT)
 
-    if (max_depth != -1 and max_depth != 0):
-        while (splits < max_depth):
-            highest_IG = -9999
-            for num in range(num_features):
-                X_l = []
-                X_r = []
-                Y_l = []
-                Y_r = []
-                i = 0
-                trindex_l = []
-                trindex_r = []
-
-                for row in X:
-                    # Subset Data to get L and R sides
-                    if row[num] == 0:
-                        # This row belongs in X_l
-                        X_l.append(row)
-                        Y_l.append(Y[i])
-                        trindex_l.append(i)
-                    elif row[num] == 1:
-                        # This row belongs in X_r
-                        X_r.append(row)
-                        Y_r.append(Y[i])
-                        trindex_r.append(i)
-                    
-                    i += 1
-
-                X_l = np.array(X_l)
-                X_r = np.array(X_r)
-                Y_l = np.array(Y_l)
-                Y_r = np.array(Y_r)
-
-                # Calculate probability of being on each side
-                samples_r = X_r.shape[0]
-                samples_l = X_l.shape[0]
-                total_samples = samples_l + samples_r
-
-                p_L = samples_l / total_samples
-                p_R = samples_r / total_samples
-
-                # Calculate Entropy of each side
-                h_l = calcEntropy(X_l, Y_l)
-                h_r = calcEntropy(X_r, Y_r)
-
-                # Calculate the Information Gain For the Split
-                IG = calcIG(H, p_L, p_R, h_l, h_r)
-                print("IG FOR FEATURE", num + 1, "SPLIT:", IG)
-
-                # Test if highest IG
-                if (IG > highest_IG):
-                    highest_IG = IG
-                    highest_IG_feature_num = num
-                    rindex_l = trindex_l
-                    rindex_r = trindex_r
-                    print("Feature number", num + 1, "has highest IG with a value of", IG)
-
-            # Print highest IG feature
-            print("Splitting on feature number", highest_IG_feature_num + 1)
-
-            # Take away values that have been labeled after split (if any) and recompute H() and split
-            splits += 1
-
-            # Remove Rows from split side with lowest entropy
-            print(X)
-            if h_l < h_r:
-                X = np.delete(X, rindex_l, axis=0)
-                Y = np.delete(Y, rindex_l, axis=0)
-            elif h_r < h_l:
-                X = np.delete(X, rindex_r, axis=0)
-                Y = np.delete(Y, rindex_r, axis=0)
-            else:
-                print("Shit entropy was the same on either side; control-c to stop")
-                input()
-            print(X)
-            input()
-
-            # Remove split feature from train dataset (column wise delete)
-            X = np.delete(X, highest_IG_feature_num, axis=1)
-            num_features = X.shape[1]
-
-            H = calcEntropy(X, Y)
-            print("New Dataset H() =", H)
-            
-        return("Training Complete")
-
-print(DT_train_binary(X, Y, 3))
+print(DT_train_binary(X, Y, 10))
